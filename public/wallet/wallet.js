bf3a8cde1fd84befb7cad3c5e1e175bc3104f708
David <game.net0218@gmail.com>
2022-03-13 21:47:02 +0100

    chart changes

--- public/wallet/wallet.js
+++ public/wallet/wallet.js
@@ -54,7 +54,7 @@ function drawChart() {
         },
         'backgroundColor': 'transparent',
 
-        'chartArea': {'width': '100%', 'height': '100%'}
+        'chartArea': {'width': '90%', 'height': '90%'}
 
     };
 
